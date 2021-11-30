33ab79b6e3a311d649679f9433d84627ba93913a
Mark Scheuerell <scheuerl@uw.edu>
2021-11-30 05:50:45 -0800

    completed example plot

--- report/plot_data.R
+++ report/plot_data.R
@@ -13,6 +13,7 @@ data_raw <- readr::read_csv(here("data", "raw", "cru_peer_mentoring_survey.csv")
 
 #### admin ####
 
+## admin responses
 data_admin <- data_raw %>%
   select(starts_with("Administration")) %>%
   select(!contains("format")) %>%
@@ -26,13 +27,23 @@ mentees <- (data_admin == "Mentee" | data_admin == "Mentor;Mentee") %>%
 mentors <- (data_admin == "Mentor" | data_admin == "Mentor;Mentee") %>%
   apply(2, sum, na.rm = TRUE)
 
+## matrix of data for plotting
 data_plot <- rbind(mentees, mentors)
 
+## list of topics
 topics <- data_admin %>%
   colnames() %>%
   sub(pattern = "(^.*\\[)(.*)(\\])", replacement = "\\2")
 
+## plot summary info
+par(mai = c(0.9, 2.9, 0.6, 0.1),
+    omi = rep(0.1, 4))
 barplot(data_plot, beside = TRUE,
+        col = c("dodgerblue", "indianred"), border = NA,
         names.arg = topics,
-        horiz = TRUE)
+        legend.text = TRUE,
+        args.legend = list(x = "top", inset = -0.2, bty = "n", border = NA,
+                           xpd = TRUE, cex = 0.8),
+        horiz = TRUE, las = 1, cex.names = 0.8,
+        xlab = "Number of responses")
 
